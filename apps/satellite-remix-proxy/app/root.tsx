c129c240f6fc6ea6ae8736e593adfd61672d8f9f
Giannis Katsanos <gkats@fastmail.com>
2023-02-21 13:08:10 +0200

    Remix satellite and proxy
    Added isSatellite and proxyUrl properties.

--- apps/satellite-remix-proxy/app/root.tsx
+++ apps/satellite-remix-proxy/app/root.tsx
@@ -1,39 +1,74 @@
-import type { LoaderFunction, MetaFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import { rootAuthLoader } from '@clerk/remix/ssr.server';
-import { ClerkApp, ClerkCatchBoundary } from '@clerk/remix';
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
 
-export const loader: LoaderFunction = args => {
+declare global {
+  interface Window {
+    ENV: {
+      CLERK_PUBLISHABLE_KEY: string;
+      CLERK_PROXY_URL: string;
+      CLERK_IS_SATELLITE: string;
+    };
+  }
+}
+
+export const loader: LoaderFunction = (args) => {
   return rootAuthLoader(
     args,
     ({ request }) => {
       const { user } = request;
-
-      console.log('User:', user);
-
-      return { user };
+      console.log("User:", user);
+      return json({
+        message: `Hello from the root loader :)`,
+        ENV: getBrowserEnvironment(),
+        user,
+      });
     },
-    { loadUser: true },
+    {
+      loadUser: true,
+      isSatellite: true,
+      proxyUrl:
+        typeof window !== "undefined"
+          ? window.ENV.CLERK_PROXY_URL
+          : process.env.CLERK_PROXY_URL,
+    }
   );
 };
 
 export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
+  charset: "utf-8",
+  title: "Satellite Remix app behind proxy",
+  viewport: "width=device-width,initial-scale=1",
 });
 
 export const CatchBoundary = ClerkCatchBoundary();
 
 function App() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
       <body>
         <Outlet />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+          }}
+        />
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -42,4 +77,21 @@ function App() {
   );
 }
 
-export default ClerkApp(App);
+function getBrowserEnvironment() {
+  const env = process.env;
+
+  return {
+    CLERK_PUBLISHABLE_KEY: env.CLERK_PUBLISHABLE_KEY,
+    CLERK_PROXY_URL: env.CLERK_PROXY_URL,
+    CLERK_IS_SATELLITE: env.CLERK_IS_SATELLITE,
+  };
+}
+
+export default ClerkApp(App, {
+  //@ts-ignore
+  isSatellite: true,
+  proxyUrl:
+    typeof window !== "undefined"
+      ? window.ENV.CLERK_PROXY_URL
+      : process.env.CLERK_PROXY_URL,
+});
