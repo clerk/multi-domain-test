9c24964238ff970bac68f49f3e0bd54cebccef34
Giannis Katsanos <gkats@fastmail.com>
2023-02-23 13:33:17 +0200

    Update Next.js proxy middleware
    Updated the satellite next.js proxy app middleware to simplify proxy
    config rewrites.

--- apps/satellite-next-proxy/middleware.js
+++ apps/satellite-next-proxy/middleware.js
@@ -1,30 +1,26 @@
 import { NextResponse } from "next/server";
-import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 
 const clerkFapi = process.env.CLERK_FAPI;
 const domain = process.env.NEXT_PUBLIC_CLERK_DOMAIN;
 const proxyUrl = process.env.NEXT_PUBLIC_CLERK_PROXY_URL;
-let xClerkProxyUrl = proxyUrl;
-if (!proxyUrl.startsWith("https://")) {
-  xClerkProxyUrl = new URL(
-    proxyUrl,
-    `https://${domain.replace(/^clerk\./, "")}`
-  ).toString();
-}
 const clerkSecretKey = process.env.CLERK_SECRET_KEY;
+const relativeProxyUrl = process.env.NEXT_PUBLIC_RELATIVE_CLERK_PROXY_URL;
 
 export default withClerkMiddleware((req) => {
-  if (req.nextUrl.pathname.startsWith(proxyUrl)) {
-    const path = req.nextUrl.pathname.replace(proxyUrl, "");
-    const url = new URL(path, clerkFapi);
-    url.search = req.nextUrl.search;
+  const { search, pathname } = req.nextUrl;
 
-    const reqHeaders = new Headers(req.headers);
-    reqHeaders.set("clerk-proxy-url", xClerkProxyUrl);
-    reqHeaders.set("clerk-secret-key", clerkSecretKey);
+  if (pathname.startsWith(relativeProxyUrl)) {
+    const url = new URL(pathname.replace(relativeProxyUrl, ""), clerkFapi);
+    url.search = search;
+
+    const headers = new Headers(req.headers);
+    headers.set("clerk-proxy-url", proxyUrl);
+    headers.set("clerk-secret-key", clerkSecretKey);
+    console.log(pathname, url.toString(), headers.get("clerk-proxy-url"));
     const response = NextResponse.rewrite(url, {
       request: {
-        headers: reqHeaders,
+        headers,
       },
     });
     return response;
